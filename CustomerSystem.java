2ac618b85a49afb359f152c2af172bcebc85db33
andrew <335620654@gapps.yrdsb.ca>
2021-04-05 12:36:02 -0400

    changes

--- CustomerSystem.java
+++ CustomerSystem.java
@@ -26,7 +26,7 @@ class CustomerSystem{
             if (userInput.equals(enterCustomerOption)){
                 // Only the line below may be editted based on the parameter list and how you design the method return
 		        // Any necessary variables may be added to this if section, but nowhere else in the code
-                enterCustomerInfo();
+                enterCustomerInfo(reader);
             }
             else if (userInput.equals(generateCustomerOption)) {
                 // Only the line below may be editted based on the parameter list and how you design the method return
@@ -56,7 +56,7 @@ class CustomerSystem{
     * The method may not nesessarily be a void return type
     * This method may also be broken down further depending on your algorithm
     */
-    public static void enterCustomerInfo() {
+    public static void enterCustomerInfo(Scanner reader) {
     }
     /*
     * This method may be edited to achieve the task however you like.
